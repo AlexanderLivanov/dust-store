377c4b0c87b4adf7ab666d3c214fd2b080cc1bed
AlexanderLivanov <help.underlinegroup@gmail.com>
2025-03-27 19:15:10 +0300

    nothing

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
 <?php
 // 08.03.2025 (c) Alexander Livanov
+
+/* 
+    All classes go below
+    Declaration:
+        // Small comment what does this class do
+        class ClassName {
+            
+        }
+*/
 class Database {
     
 }
